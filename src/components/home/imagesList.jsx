192fc81783a5277bec5fb280bbb060b906a112d1
juanfranfdezGit <jfranromfdez@gmail.com>
2025-04-07 15:48:11 +0200

    feat: api conection

--- src/components/home/imagesList.jsx
+++ src/components/home/imagesList.jsx
@@ -1,4 +1,21 @@
+import { useState, useEffect } from "react";
+
 export default function ImagesList({ setActive }) {
+
+    const BASE_URL = import.meta.env.VITE_BASE_URL;
+    const API_KEY = import.meta.env.VITE_API_KEY;
+
+    const [ data, setData ] = useState([]);
+
+    useEffect(() => {
+        fetch(`${BASE_URL}/photos?client_id=${API_KEY}`)
+        .then((response) => response.json())
+        .then((json) => {
+            setData(json);
+            console.log("datos recubidos:", json)
+        })
+        .catch((error) => console.error("Error al obtener datos:", error));
+    }, [BASE_URL, API_KEY])
 
     function handleImageClick() {
         setActive(true);
@@ -8,22 +25,19 @@ export default function ImagesList({ setActive }) {
         <>
             <main className="images">
                 <section className="images__container">
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/photo2.webp" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/photo2.webp" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/photo2.webp" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/photo2.webp" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/photo2.webp" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/headerBack.jfif" alt="testx"/>
-                    <img onClick={handleImageClick} className="images__container__imgs" src="/assets/images/photo2.webp" alt="testx"/>
+                {data.length > 0 ? (
+                    data.map((item, index) => (
+                        <img
+                        key={index}
+                        onClick={handleImageClick}
+                        className="images__container__imgs"
+                        src={item.urls.full}
+                        alt={item.user.name}
+                        />
+                    ))
+                    ) : (
+                    <p>Cargando imágenes...</p>
+                    )}
                 </section>
             </main>
         </>
